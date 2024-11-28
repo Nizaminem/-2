a06287d4a198f8a08fbb5019e6d2838521999e8a
Nizaminem <nagarizaev1994@gmail.com>
2024-11-28 17:35:45 +0300

    Модуль 2 - допзадание

---
+++ ShriftDrevniy.py
@@ -0,0 +1,8 @@
+n = int(input("Введите чило от 3 до 20: "))
+result: str = ''
+for a in range(1, 21):
+    for b in range(a + 1, 21):
+        if n % (a + b) == 0:
+            result += str(a) + str(b)
+
+print(result)
